33299ac7a8d8a3cc26d2583748c4c2ce330f0f99
Kyoung-min Park <67535039+Kyoung-M1N@users.noreply.github.com>
2024-07-15 14:01:23 +0900

    chore : 베포 스크립트 수정
    베포 스크립트 수정:
    - 배포 대상 경로 수정

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -1,8 +1,9 @@
 REPOSITORY=/home/ubuntu/app
+JAR_NAME=$(ls -tr $REPOSITORY/*SNAPSHOT.jar | tail -n 1)
 
 echo "> 현재 구동 중인 애플리케이션 pid 확인"
 
-CURRENT_PID=$(pgrep -fla java | grep hayan | awk '{print $1}')
+CURRENT_PID=$(pgrep -fla java | grep $JAR_NAME | awk '{print $1}')
 
 echo "현재 구동 중인 애플리케이션 pid: $CURRENT_PID"
 
@@ -15,11 +16,10 @@ else
 fi
 
 echo "> 새 애플리케이션 배포"
-JAR_NAME=$(ls -tr $REPOSITORY/*SNAPSHOT.jar | tail -n 1)
 echo "> JAR NAME: $JAR_NAME"
 
 echo "> $JAR_NAME 에 실행권한 추가"
 chmod +x $JAR_NAME
 
 echo "> $JAR_NAME 실행"
-nohup java -jar -Duser.timezone=Asia/Seoul $JAR_NAME >> $REPOSITORY/nohup.out 2>&1 &
+nohup java -jar -Duser.timezone=Asia/Seoul $JAR_NAME &
